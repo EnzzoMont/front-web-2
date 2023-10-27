19c97a8f33f7583953ceec1dfd3575add0a3bdc8
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-27 11:48:12 -0300

    mais um commit

---
+++ js/dom.js
@@ -0,0 +1,9 @@
+// Criando variável
+const tirarDiv = document.querySelector(".tirarDiv");
+const voltarDiv = document.querySelector(".voltarDiv");
+const card = document.querySelector(".card")
+
+// criando funções
+tirarDiv.addEventListener("click", function(){
+    alert("olá");
+})
